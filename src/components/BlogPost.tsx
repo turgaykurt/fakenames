b48935fa18c7b87dd60a7df094dd2b56dbdf5a01
turgaykurt <turgaykurt@Turgay-MacBook-Pro.local>
2024-06-30 16:56:23 +0300

    start

--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -43,65 +43,67 @@ const BlogPost: FC<BlogPostProps> = ({ post }) => {
 
     return (
         <section className="blogpost">
-            {/* post varsa içeriği göster */}
-            {post && (
-                <>
-                    <h1>{post.baslik}</h1>
-                    <p>{post.aciklama}</p>
+            <div className="blogpostic">
+                {/* post varsa içeriği göster */}
+                {post && (
+                    <>
+                        <h1>{post.baslik}</h1>
+                        <p>{post.aciklama}</p>
 
-                    <div className="butonlar">
-                        <button
-                            className={`buton ${
-                                filter === "all" ? "active" : ""
-                            }`}
-                            onClick={() => setFilter("all")}
-                        >
-                            All
-                        </button>
-                        <button
-                            className={`buton ${
-                                filter === "woman" ? "active" : ""
-                            }`}
-                            onClick={() => setFilter("woman")}
-                        >
-                            Woman
-                        </button>
-                        <button
-                            className={`buton ${
-                                filter === "man" ? "active" : ""
-                            }`}
-                            onClick={() => setFilter("man")}
-                        >
-                            Man
-                        </button>
-                    </div>
-
-                    {copiedName && (
-                        <div className="alarm">{copiedName} copied!</div>
-                    )}
-
-                    <ol>
-                        {filteredPeople.map((isim, index) => (
-                            <li
-                                key={index}
-                                className={isim.gender}
-                                onClick={() => copyToClipboard(isim.name)}
+                        <div className="butonlar">
+                            <button
+                                className={`buton ${
+                                    filter === "all" ? "active" : ""
+                                }`}
+                                onClick={() => setFilter("all")}
                             >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    height="20px"
-                                    viewBox="0 -960 960 960"
-                                    width="20px"
-                                    fill="currentColor"
+                                All
+                            </button>
+                            <button
+                                className={`buton ${
+                                    filter === "woman" ? "active" : ""
+                                }`}
+                                onClick={() => setFilter("woman")}
+                            >
+                                Woman
+                            </button>
+                            <button
+                                className={`buton ${
+                                    filter === "man" ? "active" : ""
+                                }`}
+                                onClick={() => setFilter("man")}
+                            >
+                                Man
+                            </button>
+                        </div>
+
+                        {copiedName && (
+                            <div className="alarm">{copiedName} copied!</div>
+                        )}
+
+                        <ol>
+                            {filteredPeople.map((isim, index) => (
+                                <li
+                                    key={index}
+                                    className={isim.gender}
+                                    onClick={() => copyToClipboard(isim.name)}
                                 >
-                                    <path d="M362.31-260q-27.01 0-45.66-18.65Q298-297.3 298-324.31v-455.38q0-27.01 18.65-45.66Q335.3-844 362.31-844h359.38q27.01 0 45.66 18.65Q786-806.7 786-779.69v455.38q0 27.01-18.65 45.66Q748.7-260 721.69-260H362.31Zm0-52h359.38q4.62 0 8.46-3.85 3.85-3.84 3.85-8.46v-455.38q0-4.62-3.85-8.46-3.84-3.85-8.46-3.85H362.31q-4.62 0-8.46 3.85-3.85 3.84-3.85 8.46v455.38q0 4.62 3.85 8.46 3.84 3.85 8.46 3.85Zm-124 176q-27.01 0-45.66-18.65Q174-173.3 174-200.31v-507.38h52v507.38q0 4.62 3.85 8.46 3.84 3.85 8.46 3.85h411.38v52H238.31ZM350-312v-480 480Z" />
-                                </svg>
-                                {isim.name}
-                            </li>
-                        ))}
-                    </ol>
-                </>
-            )}
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        height="20px"
+                                        viewBox="0 -960 960 960"
+                                        width="20px"
+                                        fill="currentColor"
+                                    >
+                                        <path d="M362.31-260q-27.01 0-45.66-18.65Q298-297.3 298-324.31v-455.38q0-27.01 18.65-45.66Q335.3-844 362.31-844h359.38q27.01 0 45.66 18.65Q786-806.7 786-779.69v455.38q0 27.01-18.65 45.66Q748.7-260 721.69-260H362.31Zm0-52h359.38q4.62 0 8.46-3.85 3.85-3.84 3.85-8.46v-455.38q0-4.62-3.85-8.46-3.84-3.85-8.46-3.85H362.31q-4.62 0-8.46 3.85-3.85 3.84-3.85 8.46v455.38q0 4.62 3.85 8.46 3.84 3.85 8.46 3.85Zm-124 176q-27.01 0-45.66-18.65Q174-173.3 174-200.31v-507.38h52v507.38q0 4.62 3.85 8.46 3.84 3.85 8.46 3.85h411.38v52H238.31ZM350-312v-480 480Z" />
+                                    </svg>
+                                    {isim.name}
+                                </li>
+                            ))}
+                        </ol>
+                    </>
+                )}
+            </div>
         </section>
     );
 };
